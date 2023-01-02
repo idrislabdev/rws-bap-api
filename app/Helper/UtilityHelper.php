fec815a00906b8e75ae8447dc33c78f29cc84fb5
idrislabdev <idrislabdev@gmail.com>
2023-01-02 17:08:45 +0700

    [update] nomor

--- app/Helper/UtilityHelper.php
+++ app/Helper/UtilityHelper.php
@@ -6,46 +6,50 @@ use Illuminate\Support\Facades\DB;
 
 class UtilityHelper
 {
-    public static function penyebut($nilai) {
+    public static function penyebut($nilai)
+    {
         $nilai = abs($nilai);
         $huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
         $temp = "";
         if ($nilai < 12) {
-            $temp = " ". $huruf[$nilai];
-        } else if ($nilai <20) {
-            $temp = self::penyebut($nilai - 10). " belas";
+            $temp = " " . $huruf[$nilai];
+        } else if ($nilai < 20) {
+            $temp = self::penyebut($nilai - 10) . " belas";
         } else if ($nilai < 100) {
-            $temp = self::penyebut($nilai/10)." puluh". self::penyebut($nilai % 10);
+            $temp = self::penyebut($nilai / 10) . " puluh" . self::penyebut($nilai % 10);
         } else if ($nilai < 200) {
             $temp = " seratus" . self::penyebut($nilai - 100);
         } else if ($nilai < 1000) {
-            $temp = self::penyebut($nilai/100) . " ratus" . self::penyebut($nilai % 100);
+            $temp = self::penyebut($nilai / 100) . " ratus" . self::penyebut($nilai % 100);
         } else if ($nilai < 2000) {
             $temp = " seribu" . self::penyebut($nilai - 1000);
         } else if ($nilai < 1000000) {
-            $temp = self::penyebut($nilai/1000) . " ribu" . self::penyebut($nilai % 1000);
+            $temp = self::penyebut($nilai / 1000) . " ribu" . self::penyebut($nilai % 1000);
         } else if ($nilai < 1000000000) {
-            $temp = self::penyebut($nilai/1000000) . " juta" . self::penyebut($nilai % 1000000);
+            $temp = self::penyebut($nilai / 1000000) . " juta" . self::penyebut($nilai % 1000000);
         } else if ($nilai < 1000000000000) {
-            $temp = self::penyebut($nilai/1000000000) . " milyar" . self::penyebut(fmod($nilai,1000000000));
+            $temp = self::penyebut($nilai / 1000000000) . " milyar" . self::penyebut(fmod($nilai, 1000000000));
         } else if ($nilai < 1000000000000000) {
-            $temp = self::penyebut($nilai/1000000000000) . " trilyun" . self::penyebut(fmod($nilai,1000000000000));
-        }     
+            $temp = self::penyebut($nilai / 1000000000000) . " trilyun" . self::penyebut(fmod($nilai, 1000000000000));
+        }
         return $temp;
     }
-   
-    public static function terbilang($nilai) {
-       if($nilai<0) {
-           $hasil = "minus ". trim(self::penyebut($nilai));
-       } else {
-           $hasil = trim(self::penyebut($nilai));
-       }     		
-       return $hasil;
+
+    public static function terbilang($nilai)
+    {
+        if ($nilai < 0) {
+            $hasil = "minus " . trim(self::penyebut($nilai));
+        } else {
+            $hasil = trim(self::penyebut($nilai));
+        }
+        return $hasil;
     }
 
-    public static function checkEvident($wo_id, $wo_site_id) {
-        $data = DB::table(DB::raw('tr_wo_sites tr, tr_wos trw, ma_penggunas p')) 
-                    ->select(DB::raw("tr.*, trw.lampiran_url,  
+    public static function checkEvident($wo_id, $wo_site_id)
+    {
+        $data = DB::table(DB::raw('tr_wo_sites tr, tr_wos trw, ma_penggunas p'))
+            ->select(
+                DB::raw("tr.*, trw.lampiran_url,  
                                         (SELECT count(*) 
                                             FROM 
                                                 tr_wo_site_lvs t
@@ -141,25 +145,26 @@ class UtilityHelper
                                                         tr.wo_id = dh.wo_id 
                                                     AND 
                                                         tr.wo_site_id = dh.wo_site_id) as pr_dual_homing"),
-                                        )
-                    ->where('tr.wo_id', $wo_id)
-                    ->where('tr.wo_site_id', $wo_site_id)
-                    ->whereRaw("p.id = tr.dibuat_oleh")
-                    ->whereRaw("tr.wo_id = trw.id")
-                    ->first();
-        
+            )
+            ->where('tr.wo_id', $wo_id)
+            ->where('tr.wo_site_id', $wo_site_id)
+            ->whereRaw("p.id = tr.dibuat_oleh")
+            ->whereRaw("tr.wo_id = trw.id")
+            ->first();
+
         return $data;
     }
 
-    public static function checkNomorDokumen() {
+    public static function checkNomorDokumen()
+    {
         $data_id = DB::table('ma_nomor_dokumens')
             ->select(DB::raw('max(SUBSTRING(no_dokumen, 6, 4)) as result'))
+            ->whereYear('tgl_dokumen', date('Y'))
             ->first();
 
         $counter = ($data_id) ? (int)$data_id->result + 1 : 1;
         $id = 'TEL. ' . sprintf("%04d", $counter);
-        
+
         return $id . '/YN.000/DR5-11000000/' . date('Y');
     }
-
 }
