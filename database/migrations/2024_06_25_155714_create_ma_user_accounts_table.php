c94d8c7e21d36cf5aa4e34f792eaee9d3f752f73
mochamad rifai <idrislabdev@gmail.com>
2024-06-25 17:14:36 +0700

    [update] migration user account

---
+++ database/migrations/2024_06_25_155714_create_ma_user_accounts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaUserAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ma_user_accounts', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('nama', 100);
+            $table->date('tgl_lahir');
+            $table->string('nik', 10);
+            $table->enum('status_pegawai', ['ORGANIK', 'NON-ORGANIK']);
+            $table->string('email', 100);
+            $table->string('jabatan', 100);
+            $table->string('unit', 100);
+            $table->string('site_witel', 50);
+            $table->string('datel', 50);
+            $table->string('plaza', 50);
+            $table->string('divisi', 100);
+            $table->string('telegram_id', 100);
+            $table->string('telegram_user', 100);
+            $table->string('channel', 100);
+            $table->string('nama_atasan', 100);
+            $table->string('nik_atasan', 10);
+            $table->string('jabatan_atasan', 100);
+            $table->string('file_pakta_url');
+            $table->string('image_ktp_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ma_user_accounts');
+    }
+}
